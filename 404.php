7145ec3d90d2a8e883dcf72adce602fe7c1badfd
panpod <panpod@gmail.com>
2016-02-06 10:21:23 +0530

    added style

--- 404.php
+++ 404.php
@@ -3,6 +3,12 @@
 <!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
 <!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<style type="text/css">
+.body1{
+   text-align: center;
+}
+</style>
+
 <head>
 	<?php require_once 'blocks/head.php'; ?>
 </head>
